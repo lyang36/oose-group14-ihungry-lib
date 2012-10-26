6e81b3b5ed34d618197fad33b73779f0c3191b04
SuNFloWer <SuNFloWer@sunbook.wireless.jhu.edu>
2012-10-26 16:25:35 -0400

    Change Test 1 10-26-16:25

--- iHungryLib/src/edu/jhu/cs/oose/fall2012/group14/ihungry/internet/InternetUtilImpl.java
+++ iHungryLib/src/edu/jhu/cs/oose/fall2012/group14/ihungry/internet/InternetUtilImpl.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class InternetUtilImpl implements InternetUtil {
-	Socket socket;
+	Socket socket; // socket
 	DataOutputStream out;
  	DataInputStream in;
  	//BufferedReader dr;
